d40060759dbbe226368d6b0664673d38d2742ee9
gyeo009 <gyeo009@gmail.com>
2023-03-23 17:50:57 +0900

    03_23_2

--- Project1/0x0D_15683_감시.cpp
+++ Project1/0x0D_15683_감시.cpp
@@ -1,28 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, cnt = 0;
-int board[8][8];
 
-void rotate() { // 90도 회전
+int m, n;
+int dx[4] = { 1, 0, -1, 0 }; // 남 서 북 동
+int dy[4] = { 0, -1, 0, 1 };
+int board1[8][8];
+int board2[8][8];
+vector<pair<int, int>> cctv;
 
-}
-void _1(int x, int y) {
-	for (int i = y; i < m; i++) {
-		if (board[x][i] == 6) break;
-		board[x][i] = 1;
-		cnt++;
-	}
+void upd(int x, int y, int dir) {
+
 }
 
 
 int main() {
 	cin >> n >> m;
-	
-	// board 0으로 초기화 해야되나?
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++) {
+			cin >> board1[i][j];
+			if (board1[i][j] == 0 || board1[i][j] == 6) continue;
+			cctv.push_back({ i,j });
+		}
 
+	for (int tmp = 0; tmp < (1 << (2 * cctv.size())); tmp++) {
+		for (int i = 0; i < n; i++){
+			for (int j = 0; j < m; j++) {
+				board2[i][j] = board1[i][j];
+			}
+		}
+		int brute = tmp;
+		for (int i = 0; i < cctv.size(); i++) {
+			int dir = brute % 4;
+			brute /= 4;
+			int x = cctv[i].first;
+			int y = cctv[i].second;
+			if (board2[x][y] == 1) {
 
+			}
+		}
+	}
 
-
+			
 
 	return 0;
 }
